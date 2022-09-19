8ba44df10d399c6c67a67c5810e5ae5fec0748c6
ivan2798 <inge1998@hotmail.com>
2022-09-19 10:14:27 -0500

    arreglos

---
+++ app.js
@@ -0,0 +1,16 @@
+let a;
+const PI = 3.1416;
+const NUEVE = 9;
+console.log(PI);
+a = "kenai";
+console.log(a);
+
+let objeto = {
+    nombre:"Jon",
+    edad:35
+}
+
+let colores = ["blanco","negro","azul"];
+
+console.log(objeto);
+console.log(colores)
